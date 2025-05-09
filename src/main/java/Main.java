2f00bec8d7f82e05f3e916ba4d19ff8335a60e62
kazyu0 <kazumakimijima.sb@gmail.com>
2025-05-09 06:24:57 +0000

    比較演算子

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,11 @@
 
 public class Main {
   public static void main(String[] args) {
-    int radius;
-    radius = 5;
-    System.out.println(radius * radius * 3.14);
+    System.out.println(1000 + 2000);
+    System.out.println(3000 - 1500);
+    System.out.println(50 * 40);
+    System.out.println(600 / 15);
+    System.out.println(5 % 2);
   }
 
   // @Test
