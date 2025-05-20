98b533921e5aac6044a160f762eb320955d70022
kazyu0 <kazumakimijima.sb@gmail.com>
2025-05-20 05:51:41 +0000

    if

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,9 @@
-import java.util.ArrayList;
-
 class Main {
   public static void main(String[] args) {
-    ArrayList<Integer> scores = new ArrayList<Integer>();
+    int value = 3;
 
-    scores.add(1);
-    scores.add(5);
-    scores.add(10);
-    scores.add(15);
-
-    System.out.println(scores.get(0));
-    System.out.println(scores.get(1));
-    System.out.println(scores.get(2));
-    System.out.println(scores.get(3));
+    if (value > 0){
+      System.out.println("値は正です"); 
+    }
   }
 }
