441e235713797fb703c3f0767151f7e6c12149e3
kazyu0 <kazumakimijima.sb@gmail.com>
2025-05-23 01:18:36 +0000

    mainメソッド

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,10 @@
-import java.util.ArrayList;
-
 class Main {
-  public static void main(String[] args) {
-    ArrayList<Integer> scores = new ArrayList<Integer>();
+  public static void main(String[] args) {  
+    sayHello();
+  }
 
-    scores.add(1);
-    scores.add(5);
-    scores.add(10);
-    scores.add(15);
-
-    System.out.println(scores.get(0));
-    System.out.println(scores.get(1));
-    System.out.println(scores.get(2));
-    System.out.println(scores.get(3));
+  public static void sayHello() {
+    System.out.println("Hello World");
+    return;
   }
 }
